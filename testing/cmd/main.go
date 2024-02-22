88f8bb045c0bc1ef799cf6c63cbcd20dabc8b1e8
Sathish-30 <sathish.amsaravanan@gmail.com>
2024-02-22 16:20:56 +0530

    Handled multiple endpoints using standard net/http package

--- testing/cmd/main.go
+++ testing/cmd/main.go
@@ -8,10 +8,25 @@ import (
 	"github.com/sathish-30/go-testing/internal/handler"
 )
 
+func loggerMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("The Url Path is %s and the method is %s", r.URL.Path, r.Method)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	http.HandleFunc("/user", handler.UserHomeRoute)
+	app := http.NewServeMux()
+	// Logger middleware
+	wrappedServer := loggerMiddleWare(app)
+
+	app.HandleFunc("/user/", handler.HandleGetUserById)
+	app.HandleFunc("/user", handler.UserHomeRoute)
+	app.HandleFunc("POST /addUser", handler.AddUserHandler)
+	app.HandleFunc("DELETE /deleteUser/", handler.DeleteUserByIdHandler)
+
 	port := fmt.Sprintf(":%d", 8000)
-	if err := http.ListenAndServe(port, nil); err != nil {
+	if err := http.ListenAndServe(port, wrappedServer); err != nil {
 		log.Fatal(err)
 	}
 }
--- testing/internal/handler/userHandler.go
+++ testing/internal/handler/userHandler.go
@@ -2,15 +2,63 @@ package handler
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/sathish-30/go-testing/internal/model"
 	"github.com/sathish-30/go-testing/internal/util"
 )
 
+var users = []model.User{
+	{1, "sathish", 21},
+	{2, "ammu", 21},
+	{3, "kaarthika", 19},
+	{4, "rowdy", 22},
+	{5, "dexter", 20},
+}
+
 func UserHomeRoute(w http.ResponseWriter, request *http.Request) {
-	userData := model.User{UserId: 1, UserName: "sathish", UserAge: 21}
 	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(userData)
+	err := json.NewEncoder(w).Encode(users)
 	util.CheckErr(err)
+}
+
+func HandleGetUserById(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.ParseInt(r.URL.Path[len("/user/"):], 10, 64)
+	util.CheckErr(err)
+	var result model.User
+	for _, user := range users {
+		if user.UserId == id {
+			result = user
+			break
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	error2 := json.NewEncoder(w).Encode(result)
+	util.CheckErr(error2)
+}
+
+func AddUserHandler(w http.ResponseWriter, r *http.Request) {
+	var newUser model.User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	log.Println(newUser)
+	util.CheckErr(err)
+	users = append(users, newUser)
+	log.Println("Added user")
+}
+
+func DeleteUserByIdHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.ParseInt(r.URL.Path[len("/deleteUser/"):], 10, 64)
+	util.CheckErr(err)
+	var index int
+	for i, user := range users {
+		if user.UserId == id {
+			index = i
+		}
+	}
+	users = append(users[:index], users[index+1:]...)
+	w.Header().Set("Content-Type", "application/json")
+	error2 := json.NewEncoder(w).Encode(users)
+	util.CheckErr(error2)
 }
